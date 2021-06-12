63355fc9ab705f87ea68e0f6cbb30f3eb7ba579f
락재영 <my0075425@naver.com>
2021-06-12 16:10:34 +0900

    1판 보스까지 작업.

--- Assets/Script/RandomMove.cs
+++ Assets/Script/RandomMove.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RandomMove : MonoBehaviour
 {
     // Start is called before the first frame update
-    private const float speed = 3;
+    private const float speed = 2;
     void Start()
     {
         StartCoroutine(ranMove());
