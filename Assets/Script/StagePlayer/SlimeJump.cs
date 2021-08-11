40582942898fe0347f5b2a3140b8f34842e7511d
GwonJaeYeong <gjy7448@virnect.com>
2021-08-11 17:50:10 +0900

    쨤푸 구현

---
+++ Assets/Script/StagePlayer/SlimeJump.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeJump : MonoBehaviour
+{
+    public void playerJump()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * 10f, ForceMode.Impulse);
+    }
+}
